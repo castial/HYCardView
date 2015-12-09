230c6eeab54bf2271f7f9384c50afff18974ad68
castial <897257524@qq.com>
2015-12-09 10:33:13 +0800

    Initial Commit

---
+++ HYCardView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYCardView
+//
+//  Created by hyyy on 15/12/9.
+//  Copyright © 2015年 hyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYCardView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYCardView
+//
+//  Created by hyyy on 15/12/9.
+//  Copyright © 2015年 hyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
